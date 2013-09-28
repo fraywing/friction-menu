d79392d8acbd5b6cd4dd81879e019e82a556e243
fraywing <fraywing@hotmail.com>
2013-09-28 00:05:06 -0600

    Update friction-menu.js
    added more documentation

--- friction-menu.js
+++ friction-menu.js
@@ -8,11 +8,12 @@
  *friction-menu-opts or frictionMenuOpts has to exists in the scope somewhere, either as an attribute or in a controller.
  *
  *use like:
- *<div friction-menu friction-menu-opts={JSON STUFF}></div>
+ *<div friction-menu friction-menu-opts={"watch" : ["/posts/","/coolPlace/"], "showWhenScrolledTo" : "300px"}></div>
  *
- *watch ARRAY paths to match when to show the menu.  i,e watch : ["/posts/"]
- *defaultVisible BOOLEAN Whether or no to show of collapse the friction menu on load
- *showWhenScrolledTo BOOL OR STRING if you put a pixel amount (i,e 300px) the menu wont appear until you scroll to that point, false for disabled
+ Options:
+ *<watch> ARRAY paths to match when to show the menu.  i,e watch : ["/posts/"]
+ *<defaultVisible> BOOLEAN Whether or no to show of collapse the friction menu on load
+ *<showWhenScrolledTo> BOOL OR STRING if you put a pixel amount (i,e 300px) the menu wont appear until you scroll to that point, false for disabled
  *
  *TODO:
  *	1. Add more options
